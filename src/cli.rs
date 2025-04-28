62ce80b31e8100d1508a746b7fa593a5016a3838
AmossGuy <yellowilya@gmail.com>
2025-04-28 19:43:22 -0400

    add support for processing multiple pak files sequentially

--- src/cli.rs
+++ src/cli.rs
@@ -15,19 +15,19 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-	#[command(arg_required_else_help = true, visible_alias = "l", about = "List the contents of a .pak file")]
+	#[command(visible_alias = "l", about = "List the contents of one or more .pak files")]
 	List {
-		#[arg(value_name = "PAK", help = "Path to the .pak file")]
-		pak_path: String,
+		#[arg(num_args = 1.., required = true, value_name = "PAK", help = "Path to the .pak file(s)")]
+		pak_paths: Vec<String>,
 	},
-	#[command(arg_required_else_help = true, visible_alias = "x", about = "Extract the contents of a .pak file")]
+	#[command(visible_alias = "x", about = "Extract the contents of one or more .pak files")]
 	Extract {
-		#[arg(value_name = "PAK", help = "Path to the .pak file")]
-		pak_path: String,
-		#[arg(value_name = "DEST", help = "Directory to extract files to - omit to use working directory")]
+		#[arg(num_args = 1.., required = true, value_name = "PAK", help = "Path to the .pak file(s)")]
+		pak_paths: Vec<String>,
+		#[arg(short = 'd', long = "dest", value_name = "DEST", help = "Directory to extract files to - omit to use working directory")]
 		dest_path: Option<String>,
 	},
-	#[command(arg_required_else_help = true, about = "Extract text from a .stl file")]
+	#[command(about = "Extract text from a .stl file")]
 	DumpLoctext {
 		#[arg(value_name = "STL", help = "Path to the .stl file")]
 		stl_path: String,
@@ -40,34 +40,42 @@ pub fn cli_main() -> anyhow::Result<()> {
 	let args = Cli::parse();
 	
 	match args.command {
-		Commands::List { pak_path } => {
-			let mut reader = BufReader::new(File::open(pak_path)?);
-			let index = PakIndex::create_index(&mut reader)?;
-			for entry in index.files {
-				let name = String::from_utf8_lossy(entry.0.to_bytes());
-				println!("{} ({} bytes)", name, entry.1.data_length);
+		Commands::List { pak_paths } => {
+			for pak_path in pak_paths {
+				println!("FILE: {}", pak_path);
+				
+				let mut reader = BufReader::new(File::open(pak_path)?);
+				let index = PakIndex::create_index(&mut reader)?;
+				for entry in index.files {
+					let name = String::from_utf8_lossy(entry.0.to_bytes());
+					println!("{} ({} bytes)", name, entry.1.data_length);
+				}
 			}
 		},
-		Commands::Extract { pak_path, dest_path } => {
-			let dest_path = match dest_path {
-				Some(string) => PathBuf::from(string),
-				None => std::env::current_dir()?,
-			};
-			
-			let mut reader = BufReader::new(File::open(pak_path)?);
-			let index = PakIndex::create_index(&mut reader)?;
-			for entry in index.files {
-				let name = String::from_utf8_lossy(entry.0.to_bytes());
-				println!("Extracting {}...", name);
-				let looksee = pak::read_whole_file(&entry.1, &mut reader)?;
-				let save_location = dest_path.join(name.to_string());
-				if let Some(save_dir) = save_location.parent() {
-					std::fs::create_dir_all(save_dir)?;
+		Commands::Extract { pak_paths, dest_path } => {
+			for pak_path in pak_paths {
+				println!("FILE: {}", pak_path);
+				
+				let dest_path = match dest_path {
+					Some(ref string) => PathBuf::from(string),
+					None => std::env::current_dir()?,
+				};
+				
+				let mut reader = BufReader::new(File::open(pak_path)?);
+				let index = PakIndex::create_index(&mut reader)?;
+				for entry in index.files {
+					let name = String::from_utf8_lossy(entry.0.to_bytes());
+					println!("Extracting {}...", name);
+					let looksee = pak::read_whole_file(&entry.1, &mut reader)?;
+					let save_location = dest_path.join(name.to_string());
+					if let Some(save_dir) = save_location.parent() {
+						std::fs::create_dir_all(save_dir)?;
+					}
+					let mut output_file = File::create(save_location)?;
+					output_file.write_all(&looksee)?;
 				}
-				let mut output_file = File::create(save_location)?;
-				output_file.write_all(&looksee)?;
+				println!("Done.");
 			}
-			println!("Done.");
 		},
 		Commands::DumpLoctext { stl_path, dest_path } => {
 			let mut reader = BufReader::new(File::open(stl_path)?);
