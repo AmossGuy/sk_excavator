bc84b569cea200984672f9a18a6ea516aec92ef6
AmossGuy <yellowilya@gmail.com>
2026-01-02 19:45:31 -0500

    terrible code but it's enough to look at a png inside a pak

--- godot/ui/file_views/pak.gd
+++ godot/ui/file_views/pak.gd
@@ -1,6 +1,31 @@
 extends VBoxContainer
 
+@onready var item_list: ItemList = $ItemList
 var resource: SkePak
 
+func _ready() -> void:
+	item_list.item_activated.connect(_item_list_item_activated)
+
 func file_view_open(_path: String) -> void:
-	$TextEdit.text = "\n".join(resource.file_names)
+	item_list.clear()
+	for file_name in resource.get_file_names():
+		item_list.add_item(file_name)
+
+func _item_list_item_activated(index: int) -> void:
+	var file := item_list.get_item_text(index)
+	if file.get_extension() == "png":
+		var data: PackedByteArray = resource.read_archived_file(file)
+		var image := Image.new()
+		image.load_png_from_buffer(data)
+		var texture := ImageTexture.create_from_image(image)
+		
+		var window := Window.new()
+		window.close_requested.connect(window.queue_free)
+		var sprite := Sprite2D.new()
+		sprite.texture = texture
+		sprite.centered = false
+		sprite.scale = Vector2(3, 3)
+		window.add_child(sprite)
+		add_child(window)
+	else:
+		push_error("not a png ngl")
--- rust/src/godot/format_resources.rs
+++ rust/src/godot/format_resources.rs
@@ -1,11 +1,13 @@
 use godot::prelude::*;
 use godot::classes::file_access::ModeFlags;
 
+use crate::formats::pak::{self, PakIndex};
+
 #[derive(GodotClass)]
 #[class(no_init, base=Resource)]
 pub struct SkePak {
-	#[var(get)]
-	file_names: PackedArray<GString>,
+	file: GFile,
+	index: PakIndex,
 	base: Base<Resource>,
 }
 
@@ -14,16 +16,26 @@ impl SkePak {
 	#[func]
 	pub fn open_file(path: GString) -> Gd<Self> {
 		Gd::from_init_fn(|base| {
-			let mut reader = GFile::open(&path, ModeFlags::READ).unwrap();
-			let index = crate::formats::pak::PakIndex::create_index(&mut reader).unwrap();
-			let file_names = index.files.into_iter().map(|f| {
-				GString::try_from_cstr(&f.0, Encoding::Utf8).unwrap()
-			}).collect();
+			let mut file = GFile::open(&path, ModeFlags::READ).unwrap();
+			let index = PakIndex::create_index(&mut file).unwrap();
 			
-			Self {
-				file_names,
-				base,
-			}
+			Self { file, index, base }
 		})
+	}
+	
+	#[func]
+	pub fn get_file_names(&self) -> PackedArray<GString> {
+		self.index.files.iter().map(|f| {
+			GString::try_from_cstr(&f.0, Encoding::Utf8).unwrap()
+		}).collect()
+	}
+	
+	#[func]
+	pub fn read_archived_file(&mut self, name: GString) -> PackedArray<u8> {
+		let file_entry = self.index.files.iter().find(|entry| {
+			GString::try_from_cstr(&entry.0, Encoding::Utf8).unwrap() == name
+		}).unwrap();
+		let data = pak::read_whole_file(&file_entry.1, &mut self.file).unwrap();
+		data.as_slice().into()
 	}
 }
