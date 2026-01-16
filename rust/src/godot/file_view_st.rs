378c8f9fbefc9252b6b780495c07e7394017afb1
AmossGuy <yellowilya@gmail.com>
2026-01-15 21:49:50 -0500

    load gradually in process function. this is better, but not that much

--- rust/src/godot/file_view_st.rs
+++ rust/src/godot/file_view_st.rs
@@ -1,17 +1,26 @@
 use godot::prelude::*;
-use godot::classes::{Tree, TreeItem};
+use godot::classes::{Tree, ITree};
 
 use std::error::Error;
 use std::io::Cursor;
 
 use crate::filesystem::cruddy_complex_load;
-use crate::formats::st::read_st;
+use crate::formats::st::{read_st, StReadOutcome};
 use crate::godot::browser_tree::ItemSource;
 
 #[derive(GodotClass)]
 #[class(init, base=Tree)]
 pub struct FileViewSt {
 	base: Base<Tree>,
+	stuff: Option<StReadOutcome>,
+	loaded_items: usize,
+}
+
+#[godot_api]
+impl ITree for FileViewSt {
+	fn process(&mut self, _delta: f64) {
+		self.load_strings(10);
+	}
 }
 
 impl FileViewSt {
@@ -25,16 +34,39 @@ impl FileViewSt {
 		self.base_mut().set_columns(field_count as i32);
 		let _root = self.base_mut().create_item().unwrap();
 		
-		let chunks = stuff.strings.chunks(field_count);
-		for (i, chunk) in chunks.enumerate() {
-			if i == 0 {
-				for (j, column_name) in chunk.iter().enumerate() {
-					self.base_mut().set_column_title(j as i32, column_name);
-				}
-			} else {
-				self.base_mut().create_item().unwrap();
-			}
-		}
+		self.stuff = Some(stuff);
 		Ok(())
+	}
+	
+	fn load_strings(&mut self, amount: usize) {
+		// Simple way to keep the borrow checker happy
+		let Some(stuff) = self.stuff.take() else { return; };
+		
+		let Some(mut root) = self.base().get_root() else { return; };
+		let field_count = stuff.field_count;
+		
+		let mut chunks = stuff.strings
+			.chunks(field_count)
+			.skip(self.loaded_items)
+			.take(amount);
+		
+		if self.loaded_items == 0 {
+			let chunk = chunks.next().unwrap_or_default();
+			for (j, text) in chunk.into_iter().enumerate() {
+				self.base_mut().set_column_title(j as i32, text);
+			}
+			self.loaded_items += 1;
+		}
+		
+		for chunk in chunks {
+			let mut item = root.create_child().unwrap();
+			for (j, text) in chunk.into_iter().enumerate() {
+				item.set_text(j as i32, text);
+			}
+			self.loaded_items += 1;
+		}
+		
+		// Simple way to keep the borrow checker happy, cont
+		self.stuff = Some(stuff)
 	}
 }
