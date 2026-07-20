324aa6bd0cdb9159bb337d8dc74d21816241e68f
AmossGuy <yellowilya@gmail.com>
2026-07-20 10:12:03 -0400

    make said reflected fields editable

--- excavator_egui/src/file_view/anb.rs
+++ excavator_egui/src/file_view/anb.rs
@@ -2,7 +2,7 @@ use super::{FileView, FileViewEffect};
 use std::io::{BufRead, Seek};
 
 use bevy_ecs::{entity::Entity, reflect::AppTypeRegistry, world::World};
-use bevy_reflect::{PartialReflect, ReflectRef, TypeRegistry, structs::Struct};
+use bevy_reflect::{PartialReflect, ReflectMut, structs::Struct};
 
 pub struct AnbFileView {
 	ecs_world: World,
@@ -25,49 +25,52 @@ impl AnbFileView {
 
 impl FileView for AnbFileView {
 	fn ui(&mut self, ui: &mut egui::Ui) -> FileViewEffect {
-		entity_ui(ui, &self.ecs_world, self.root);
+		entity_ui(ui, &mut self.ecs_world, self.root);
 		FileViewEffect::default()
 	}
 }
 
-fn entity_ui(ui: &mut egui::Ui, world: &World, entity: Entity) {
-	let entity_ref = world.get_entity(entity)
-		.expect("entity should be spawned");
-	let component_list = entity_ref.archetype().components();
+fn entity_ui(ui: &mut egui::Ui, world: &mut World, entity: Entity) {
+	let component_list = {
+		let entity_ref = world.get_entity(entity)
+			.expect("entity should be spawned");
+		entity_ref.archetype().components().to_vec()
+	};
 	
-	for &component_id in component_list {
+	for component_id in component_list {
 		if let Some(info) = world.components().get_info(component_id) && let Some(type_id) = info.type_id() {
-			let reflect = world.get_reflect(entity, type_id)
+			let mut reflect = world.get_reflect_mut(entity, type_id)
 				.expect("entity should have the components of its archetype");
-			let registry = world.get_resource::<AppTypeRegistry>()
-				.expect("type registry should exist")
-				.read();
 			
-			match reflect.reflect_ref() {
-				ReflectRef::Struct(thing) => { struct_ui(ui, thing, &registry); },
+			match reflect.reflect_mut() {
+				ReflectMut::Struct(thing) => { struct_ui(ui, thing); },
 				_ => {},
 			}
 		}
 	}
 }
 
-fn struct_ui(ui: &mut egui::Ui, thing: &dyn Struct, registry: &TypeRegistry) {
+fn struct_ui(ui: &mut egui::Ui, thing: &mut dyn Struct) {
 	ui.heading(thing.reflect_type_ident().unwrap_or("(anonymous type)"));
 	
 	egui::Grid::new("struct").show(ui, |ui| {
-		for (name, value) in thing.iter_fields() {
+		for i in 0..thing.field_len() {
+			let name = thing.name_at(i).unwrap();
 			ui.label(name);
-			match lookup_value_widget(value, registry) {
+			
+			let value = thing.field_at_mut(i).unwrap();
+			match lookup_value_widget(value) {
 				None => { ui.label("(unknown type)"); },
 				Some(value) => { value.value_widget(ui); },
 			};
+			
 			ui.end_row();
 		}
 	});
 }
 
-fn lookup_value_widget<'a>(value: &'a dyn PartialReflect, _registery: &TypeRegistry) -> Option<&'a dyn ValueWidget> {
-	if let Some(number) = value.try_downcast_ref::<u32>() {
+fn lookup_value_widget<'a>(value: &'a mut dyn PartialReflect) -> Option<&'a mut dyn ValueWidget> {
+	if let Some(number) = value.try_downcast_mut::<u32>() {
 		Some(number)
 	} else {
 		None
@@ -84,13 +87,12 @@ fn lookup_value_widget<'a>(value: &'a dyn PartialReflect, registery: &TypeRegist
 
 // #[reflect_trait]
 trait ValueWidget {
-	fn value_widget(&self, ui: &mut egui::Ui);
+	fn value_widget(&mut self, ui: &mut egui::Ui);
 }
 
 
 impl ValueWidget for u32 {
-	fn value_widget(&self, ui: &mut egui::Ui) {
-		let mut fake = self.clone();
-		ui.add(egui::DragValue::new(&mut fake));
+	fn value_widget(&mut self, ui: &mut egui::Ui) {
+		ui.add(egui::DragValue::new(self));
 	}
 }
